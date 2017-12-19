cfd146c4df24e61476202b106aa337b9eac25c2d
apocalypse575 <apocalypse575@gmail.com>
2017-12-19 23:45:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-
+	new_hash = {}
 end
 
 def actor
-
+	actor => {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
@@ -11,21 +11,29 @@ def monopoly
 
 end
 
+
+# working with hashes instantiating a hash with ONE key-value pair #actor creates a hash named acto
+# r whose key is a symbol :name and whose value is a string, 'Dwayne The Rock Johnson'
+# building a multidimensional monopoly hash adding a key-value pair to the top level of the monopol
+y hash #monopoly adds a key-value pair to the monopoly hash. The key should be :railroads and the value should be an empty hash
+# building a multidimensional monopoly hash adding key-value pairs to the second level of the monop
+#monopoly method #monopoly_with_second_tier sets the first key of the :railroads hash to a symbol, :pieces,
+ whose value is the integer 4
+rspec ./spec/intro_to_ruby_hashes_lab_spec.rb:46 # building a multidimensional monopoly hash adding key-value pairs to the second level of the monop
+oly hash that we started building in the #monopoly method #monopoly_with_second_tier sets the second key of the :railroads hash to a symbol, :names,
+ whose value is an empty hash
+
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+
 end
-
-
-
-
